e2cd768141cf2fcd05394eb345748acfba849eda
Omar Qaisi <thesaver23@gmail.com>
2021-06-27 00:32:20 +0300

    List tasks

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,6 +1,23 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity ^0.8.6;
 
 contract TodoList {
     uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool isCompleted;
+    }
+    mapping(uint => Task) public tasks;
+
+    constructor() {
+        createTask("Have a 2hr studying session about BlockChain :)");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
+
 }
